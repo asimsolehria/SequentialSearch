7a11a013fbd6d3d049dc11b1fc73ea61fb61c430
student <student@imsedu.local>
2020-10-08 13:11:56 +0500

    Added code ...

--- Search/src/search/Search.java
+++ Search/src/search/Search.java
@@ -5,17 +5,42 @@
  */
 package search;
 
+import java.util.Random;
 /**
  *
  * @author student
  */
 public class Search {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    int []array;
+    Random rand=new Random();
+    public int length=array.length;
+    public Search(int length) {
+        array=new int[length];
     }
     
+    public void fill()
+    {
+        for (int i = 0; i < this.array.length; i++) {
+            array[i]=rand.nextInt();
+        }
+        
+    }
+    public void display()
+    {
+        for (int i = 0; i < this.array.length; i++) {
+            System.out.println(this.array[i]);
+        }
+    }
+    
+    public void search(int num, int length )
+    {
+        if (this.array[length-1]==num || length<0)
+        {
+            System.out.println(this.array[length-1]);
+            return;
+        }
+        search(num,length-1);
+    }
+            
 }
